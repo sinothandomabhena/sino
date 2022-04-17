6a7263c1d3610ed193a56ecf9deaa357e0a67836
Sinothando Mabhena <53974039+sinothandomabhena@users.noreply.github.com>
2022-04-17 13:30:31 +0200

    adde about me section

--- pages/index.js
+++ pages/index.js
@@ -19,21 +19,62 @@ export default function Home() {
 
         <div className={styles.grid}>
           <div className="mt-10">
+            <h1 className="ml-5 text-2xl font-bold">/ about me</h1>
+            <div className={styles.cardabout}>
+              <p className="text-gray-700">
+                I&apos;m a software engineer based in{" "}
+                <a
+                  href="https://en.wikipedia.org/wiki/Pretoria"
+                  className="font-semibold"
+                >
+                  Pretoria, South Africa
+                </a>
+                . I have profound interest in frontend development, UI design,
+                human-computer interaction, and everything in between. I studied
+                Bsc Information & Knowledge Systems at the{" "}
+                <a
+                  href="https://www.up.ac.za/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="font-semibold"
+                >
+                  University of Pretoria
+                </a>
+              </p>
+              <p className="text-md my-3 text-gray-700">
+                Here are some technologies I have been working with:
+              </p>
+              <div className="flex flex-row">
+                <ul className="list-disc px-4 text-gray-700">
+                  <li>Swift</li>
+                  <li>React</li>
+                  <li>React Native</li>
+                </ul>
+                <ul className="list-disc px-4 mx-5 text-gray-700">
+                  <li>Next</li>
+                  <li>JavaScript</li>
+                  <li>HTML &amp; CSS</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-10">
             <h1 className="ml-5 text-2xl font-bold">/ experience</h1>
-            <p className={styles.card}>
+            <div className={styles.card}>
               <h2 className="font-semibold">
                 Junior Software Developer @{" "}
                 <a href="https://teamgeek.io/">Teamgeek</a>
               </h2>
-              <p className="text-sm mb-4 text-gray-500">Jan 2021 - Dec 2021</p>
-              <p>
+              <p className="text-md mb-4 text-gray-500">Jan 2021 - Dec 2021</p>
+              <p className="text-gray-700">
                 Developed and maintained frontend mobile responsive web
                 applications built in React. In addition to React,
                 styled-components was used for styling and also made use of
                 apollo to interact with a GraphQL API. Also worked on a cross
                 platform mobile application built in React Native
               </p>
-              <p className="text-md mt-4 text-gray-500">Worked on</p>
+              <p className="text-md mt-4 text-gray-500">Worked on:</p>
               <ul>
                 <li>
                   <a
@@ -66,29 +107,26 @@ export default function Home() {
                   >
                     bettr.app
                   </a>
-                  <span className="text-gray-400">
-                    {" "}
-                    &rarr; React Native
-                  </span>
+                  <span className="text-gray-400"> &rarr; React Native</span>
                 </li>
               </ul>
-            </p>
+            </div>
           </div>
 
-          <p className={styles.card}>
+          <div className={styles.card}>
             <h2 className="font-semibold">
               Graduate Software Engineer @{" "}
               <a href="https://www.dvt.co.za/">DVT</a>
             </h2>
-            <p className="text-sm mb-5 text-gray-500">Feb 2022 - Present</p>
-            <p>
+            <p className="text-md mb-5 text-gray-500">Feb 2022 - Present</p>
+            <p className="text-gray-700">
               Developing iOS mobile applications as part of a twelve-month
               graduate programme that provides extensive coaching. In addition
               to skills in software engineering technology, the programme
               includes soft skills coaching, training and experience in Agile,
               which is done in collaboration with the DVT Academy.
             </p>
-            <p className="text-md mt-4 text-gray-500">Worked on</p>
+            <p className="text-md mt-4 text-gray-500">Worked on:</p>
             <ul>
               <li>
                 <a
@@ -117,7 +155,7 @@ export default function Home() {
                 </span>
               </li>
             </ul>
-          </p>
+          </div>
         </div>
       </main>
 
